3d4085f64208d653e18d41fc11db89a6a9da4bf7
shassanien3 <souad@gatech.edu>
2017-06-30 17:27:54 -0700

    test file

---
+++ test.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def PredictTheWinner(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        mem = {}
+        def winner(nums,s,e):
+            if s == e:
+                return nums[s]
+            if (s,e) not in mem:
+                a =  nums[s] -  winner(nums,s+1,e)
+                b =  nums[e] - winner(nums,s,e-1)
+            mem[(s,e)] = max(a, b)
+            return mem[(s,e)]
+        return winner(nums,0,len(nums) - 1) >= 0

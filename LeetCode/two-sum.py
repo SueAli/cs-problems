89169fa8dda041c201cb83e35d940ccb293cf62e
SueAli <soaad.ali.134@gmail.com>
2023-07-25 03:13:38 +0300

    Update two-sum.py

--- LeetCode/two-sum.py
+++ LeetCode/two-sum.py
@@ -6,13 +6,30 @@
 # You can return the answer in any order.
 
 class Solution:
-  # brute force solution
-  # Time complexity => O(n*2)
-  # Space complexity => O(1)
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum1(self, nums: List[int], target: int) -> List[int]:
+        # brute force solution
+        # Time complexity => O(n*2)
+        # Space complexity => O(1)
         n = len(nums)
         for i in range (0, n):
             for j in range (i+1, n):
                 if nums[i] + nums[j] == target:
                     return [i,j]
         return []
+
+    def twoSum2(self, nums: List[int], target: int) -> List[int]:
+        # Time complexity is O(1)
+        # Space complexity is O(n) for creating the dictionary
+        n = len(nums)
+        if n != 0:
+            nums_dic = {}
+            for i in range (0, n):
+                nums_dic[nums[i]] = i
+
+            for i in range(0, n):
+                remaining = target - nums[i]
+                if nums_dic.get(remaining) is not None\
+                and nums_dic.get(remaining) != i :
+                    return [i, nums_dic.get(remaining)]
+        return []
+
